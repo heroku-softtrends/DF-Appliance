6b350d1d3aee105ef01658efbc036d5e7d21c68d
baliles <david@baliles.com>
2016-09-30 07:39:40 -0400

    Commit Before Heroku Button

--- ConfigVars.cs
+++ ConfigVars.cs
@@ -24,8 +24,8 @@ namespace DemoDFAppliance
 
             //for local testing
             //AppID = "APP000909A";
-            //IOTToken = "Bearer iguZgpnIwr8N60cD7cYgSMbYQm9QZXEY9AaqmeD6f4d2DoyvZNEhcQdzGSSFivDylWcXR5ShTu1AfMSCJi9sAj";
-            //EnpointUrl = "https://dashboard.us3.sfdcnow.com/clusters/xcdvudaz0dz3/inputstreams/appliance_sales_e001/connections/appliance_sales_i001/event";
+            //IOTToken = "Bearer 279OC5MYsEqkVwBdWqTMMixlePISTXgfvLUFJJ8luCScsIq8P4svqPfjhEhawnuzAt20KiqvT9ae36XM3YXcFU";
+            //EnpointUrl = " https://dashboard.us3.sfdcnow.com/clusters/xcdvudaz0dz3/inputstreams/appliance_sales_e001/connections/appliance_sales_i001";
             //IronMQUrl = "https://mq-aws-eu-west-1-1.iron.io/3/projects/57e6c63b0e8c650007ec1f00/queues/appliance_messages";
             //IronMQToken = "0FVb95I4FXy95vuUt3t4";
         }
